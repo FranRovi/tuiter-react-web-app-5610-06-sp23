ee7ca6f49cd796ea6ca1de1cdca3dfffc53a4cc5
Francisco Rovirosa <rovirosafrancisco@gmail.com>
2023-04-02 14:24:16 -0700

    Update services file for update likes and dislikes

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,10 +1,16 @@
 import axios from 'axios';
 const TUITS_API = 'http://localhost:4000/api/tuits';
 
-export const findTuits = async () => {
+export const findAllTuits = async () => {
     const response = await axios.get(TUITS_API);
+    // return response.data; 
     const tuits = response.data;
     return tuits;
+}
+
+export const findTuitById = async (tid) => {
+    const response = await axios.get(`${TUITS_API}/${tid}`);
+    return response.data;
 }
 
 export const deleteTuit = async (tid) => {
@@ -13,12 +19,14 @@ export const deleteTuit = async (tid) => {
 }
 
 export const createTuit = async (tuit) => {
+    //return await axios.post(TUITS_API, tuit);
     const response = await axios.post(TUITS_API, tuit);
     return response.data;
 }
 
 export const updateTuit = async (tuit) => {
-    const response = await axios.put(`${TUITS_API}/${tuit._id}`, tuit);
-    //return tuit;
+    const response = await axios.put(`${TUITS_API}/${tuit.id}`, tuit);
+    console.log(tuit);
+    console.log(response);
     return response;
 }
