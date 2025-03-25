0039095c55be4fd1b4575218244156f4a8ba13d3
Francisco Rovirosa <rovirosafrancisco@gmail.com>
2025-03-25 15:39:24 -0700

    Remove Links to lab Assigments

--- src/App.js
+++ src/App.js
@@ -9,11 +9,19 @@ function App() {
   return (
       <BrowserRouter> 
         <div className="container">
-          <Routes>
-            <Route index path="/*" element={<Labs/>}/>
-            <Route path="/hello" element={<HelloWorld/>}/>
-            <Route path="/tuiter/*" element={<Tuiter/>}/>
-          </Routes>
+          
+
+            
+            <Routes>
+            {/* <Route index path="/*" element={<Labs/>}/>
+            <Route path="/hello" element={<HelloWorld/>}/> */}
+              {/* <Route path="/tuiter/*" element={<Tuiter/>}/> */}
+            {/* <Route path="/" redirect="/tuiter/*" /> */}
+              
+              <Route path="/tuiter/*" element={<Tuiter/>}/>
+              <Route path="/" element={<Tuiter/>}/>
+            </Routes>
+
             
         </div>
       </BrowserRouter>
